f57c78d82b63186e9d39ce8a7a59316600738769
shaarang.pd <84028033+shaarang12@users.noreply.github.com>
2023-03-10 19:36:52 +0530

    Add files via upload

--- app.mjs
+++ app.mjs
@@ -3,6 +3,8 @@ import bcrypt from 'bcrypt';
 import mysql from 'mysql';
 import path from 'path';
 import url from "url";
+import session from 'express-session';
+
 // Create an Express app
 const app = express();
 const __filename = url.fileURLToPath(import.meta.url);
@@ -10,6 +12,11 @@ const __dirname = path.dirname(__filename);
 import bodyParser from 'body-parser';
 
 
+app.use(session({
+  secret: 'AutoAuthority2023',
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -34,7 +41,7 @@ console.log(__dirname);
 // Render the index page
 app.get("/", (req, res) => {
      
-  res.sendFile(path.join(__dirname+'/views/index.html'));
+  res.render("index");
 });
 app.get("/register",(req,res)=>{
   res.render("register1",{});
@@ -104,11 +111,11 @@ app.post('/register3', async (req, res) => {
   }
 });
 
-var user_id, password;
+// var user_id, password;
 app.post('/newuser', async (req, res) => {
-user_id = req.body.username;
-password = req.body.password;
-// Retrieve the hashed password value from the database based on the entered user ID.
+  const user_id = req.body.username;
+  const password = req.body.password;
+  // Retrieve the hashed password value from the database based on the entered user ID.
   const query = 'SELECT user_pass FROM user_register WHERE user_id = ?';
   pool.query(query, [user_id], async (error, results, fields) => {
     if (error) {
@@ -117,71 +124,45 @@ password = req.body.password;
     } else {
       if (results.length > 0) {
         const storedPassword = results[0].user_pass;
-        
         // Use the retrieved hashed password value and the entered plaintext password to compare them using the bcrypt.compare function.
         const isMatch = await bcrypt.compare(password, storedPassword);
-        
         if (isMatch) {
           console.log('Login Success!');
-          res.sendFile(path.join(__dirname+'/views/index.html'));
+          req.session.user_id = user_id; //store user_id in session
+          res.render("index1", {user_id: user_id});
         } else {
           console.log('Incorrect Password!');
-          res.render("login1",{msg:true});
+          res.render("login1", { msg: true });
         }
       } else {
         console.log('User not found!');
-        res.render("login1",{msg:true});
+        res.render("login1", { msg: true });
       }
     }
   });
 });
 
-// app.get("/register2", (req, res) => {
-//   res.render("register3")
+app.get('/logout', function(req, res) {
+  req.session.destroy(function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect('/login');
+    }
+  });
+});
 
-// });
+function requireLogin(req, res, next) {
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
+app.get('/index1', requireLogin, function(req, res) {
+  res.render('index');
+});
 
-//   try {
-//     const f_name = req.body.fname
-//     const l_name = req.body.lname
-//     const dob = req.body.dob
-//     const gender = req.body.gender
-
-    
-
-//   // if () {
-//   //       return res.status(400).send('Username or password is missing');
-//   //     }
-//     // const { username, password } = req.body;
-
-//     // Hash the password using bcrypt
-    
-//     const salt = bcrypt.genSaltSync(12)
-//     console.time("hash")
-//     const hash = await bcrypt.hash(user_pass, salt)
-//     console.timeEnd("hash")
-    
-//     //match a password
-//     // const isMatch = await bcrypt.compare("helloworld", hash)
-//     // console.log(isMatch)
-
-
-//     // Insert the user data into the MySQL database
-//     const query = 'INSERT INTO user_register (f_name, l_name, dob, age, gender, phone, email, address_p, address_s, total_vehicles, user_id, user_pass) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)';
-//     pool.query(query, [f_name, l_name, dob, age, gender, phone, email, address_p, address_s, total_vehicles, user_id, hash], (error, results, fields) => {
-//       if (error) {
-//         console.error('Failed to create user:', error);
-//         res.status(500).send('Failed to create user');
-//       } else {
-//         console.log('User created successfully!');
-//         res.status(200).send('User created successfully!');
-//       }
-//     });
-//   } catch (error) {
-//     console.error('Failed to create user:', error);
-//     res.status(500).send('Failed to create user');
-//   }
-// });
 
 // Start the server
 app.listen(3004, () => {
